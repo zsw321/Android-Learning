17c3771b1c55a68cd477be7f41a215bc9468a2f9
zsw321 <zhanghouwen321@gmail.com>
2014-07-24 20:14:13 +0800

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,16 +1,25 @@
 //1.首先相对布局下摆放两个TestView、两个Edit，两个按钮
 
-//1.1点击登录按钮消息框的弹出
+//1.1 学习如何获取文本框内容、设置文本框内容，弹出消息框
+//步骤：在账号编辑框输入内容，点击登录按钮，将输入的内容设置到密码编辑框， 并将内容以消息框弹出
+
 Button LoginBtn = (Button)findViewById(R.id.LoginBtn);
-		LoginBtn.setOnClickListener(new OnClickListener() {
+LoginBtn.setOnClickListener(new OnClickListener() {
 			
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-				builder.setTitle("确定");
-				builder.setMessage("该按钮被点击!");
-				builder.setPositiveButton("是", null);
-				builder.show();
+public void onClick(View v) {
+// TODO Auto-generated method stub
+	EditText LoginText = (EditText)findViewById(R.id.NameEdit);
+	strLoginName = LoginText.getText().toString();           //strLoginName为成员函数，声明为String变量
 				
-			}
-		});
+	EditText PasswdText = (EditText)findViewById(R.id.passwdEdit);
+	PasswdText.setText(strLoginName);
+				
+	AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+		builder.setTitle("确定");
+		builder.setMessage(strLoginName);
+		builder.setPositiveButton("是", null);
+		builder.show();
+				
+				
+	}
+});
